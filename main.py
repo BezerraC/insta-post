1b2d9cfa82d9ecd59e564a4fdb56237cf3ff3fcd
BezerraC <cbezerraneto@gmail.com>
2025-03-19 10:45:44 -0300

    fix: days count on workflow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from instagrapi import Client
 from dotenv import load_dotenv
 
@@ -7,18 +8,13 @@ load_dotenv()
 USERNAME = os.getenv("IG_USERNAME")
 PASSWORD = os.getenv("IG_PASSWORD")
 
-# Read and add counter
-def get_counter():
-    try:
-        with open("counter.txt", "r") as f:
-            counter = int(f.read())
-    except FileNotFoundError:
-        counter = 10
-    return counter
+START_DATE = datetime(2025, 2, 16) 
 
-def save_counter(counter):
-    with open("counter.txt", "w") as f:
-        f.write(str(counter))
+def get_day_count():
+    """Calculate how many days have passed since initial date."""
+    today = datetime.today()
+    delta = (today - START_DATE).days
+    return delta + 1 
 
 # Auth in Instagram
 def login():
@@ -40,7 +36,7 @@ def post_reel():
         cl = login()
 
         # Get the counter of the day
-        counter = get_counter()
+        counter = get_day_count()
 
         video_path = "reel.mp4"
         thumb = "reel.mp4.jpg"
@@ -50,9 +46,6 @@ def post_reel():
         cl.clip_upload(video_path, caption, thumb)
         
         print(f"Reel successfully posted! {caption}")
-
-        # Update the counter file
-        save_counter(counter + 1)
         
     except Exception as e:
         print(f"Error to post reel: {e}")
